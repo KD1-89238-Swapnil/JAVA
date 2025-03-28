2575290e3a210a03d39cc401bf2e48e762492ac5
swapnil tiwade <swapniltiwade78@gmail.com>
2025-03-28 17:16:06 +0530

    make changes

--- Assignment1.1/src/Test.java
+++ Assignment1.1/src/Test.java
@@ -1,28 +1,30 @@
-
-
 import java.util.Scanner;
 
-public class Test{
+public class NumberConverterManual {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter first number: ");
-        if (!scanner.hasNextDouble()) {
-            System.out.println("Error: Invalid input. Please enter a valid double value.");
-            return;
-        }
-        double num1 = scanner.nextDouble();
-
-        System.out.print("Enter second number: ");
-        if (!scanner.hasNextDouble()) {
-            System.out.println("Error: Invalid input. Please enter a valid double value.");
-            return;
-        }
-        double num2 = scanner.nextDouble();
-
-        double average = (num1 + num2) / 2;
-        System.out.println("The average is: " + average);
-
+        
+        System.out.print("Enter Number : ");
+        int number = scanner.nextInt();
+        
+        System.out.println("Given Number : " + number);
+        System.out.println("Binary equivalent : " + convertToBinary(number));
+        System.out.println("Octal equivalent : " + convertToOctal(number));
+        System.out.println("Hexadecimal equivalent : " + convertToHex(number));
+        
         scanner.close();
+    }
+    
+    // Manual conversion methods
+    public static String convertToBinary(int n) {
+        return Integer.toString(n, 2);
+    }
+    
+    public static String convertToOctal(int n) {
+        return Integer.toString(n, 8);
+    }
+    
+    public static String convertToHex(int n) {
+        return Integer.toString(n, 16);
     }
 }
