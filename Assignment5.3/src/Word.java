41d6a84e30c67c5d628ac3092ea81d078e8aaae5
89238-Swapnil <swapniltiwade78@gmail.com>
2025-03-28 18:39:06 +0530

    Updated

--- Assignment5.3/src/Word.java
+++ Assignment5.3/src/Word.java
@@ -19,23 +19,22 @@ public class Word {
             return 0;
         }
         
-        // Trim leading and trailing whitespace
+     
         String trimmedStr = str.trim();
-        
-        // If the trimmed string is empty after trimming, return 0
+     
         if (trimmedStr.isEmpty()) {
             return 0;
         }
         
-        // Split the string into words using one or more whitespace characters as delimiter
+        
         String[] words = trimmedStr.split("\\s+");
         
-        // Optional: Print each word (for debugging)
+      
         for (String word : words) {
             System.out.println(word);
         }
         
-        // Return the number of words
+      
         return words.length;
     }
 }
